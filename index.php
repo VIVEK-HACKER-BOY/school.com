a52df604ce2a8e94856113823f920c9b1d1fe254
VIVEK-HACKER-BOY <101443113+VIVEK-HACKER-BOY@users.noreply.github.com>
2023-02-22 04:17:45 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Vivek School</title>
+    <img src='https://i.postimg.cc/Y0L2r2kn/dcmodel112.png' border='0' alt='dcmodel112' width="960"/></a>
+
+    <style>
+      /* Style for the navigation bar */
+      .navbar {
+        overflow: hidden;
+        background-color: #2f2f2f;
+        font-family: Arial, sans-serif;
+      }
+
+      .navbar a {
+        float: left;
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 18px;
+      }
+
+      .navbar a:hover {
+        background-color: #84bdbc;
+      }
+
+      .dropdown {
+        float: left;
+        overflow: hidden;
+      }
+
+      .dropdown .dropbtn {
+        font-size: 18px;
+        border: none;
+        outline: none;
+        color: white;
+        padding: 14px 16px;
+        background-color: inherit;
+        font-family: inherit;
+        margin: 0;
+      }
+
+      .navbar a:hover, .dropdown:hover .dropbtn {
+        background-color: #111;
+      }
+
+      .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        z-index: 1;
+      }
+
+      .dropdown-content a {
+        float: none;
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+        text-align: left;
+      }
+
+      .dropdown:hover .dropdown-content {
+        display: block;
+      }
+    </style>
+  </head>
+  <body>
+    <!-- Navigation bar markup -->
+    <div class="navbar">
+      <a href="#home">Home</a>
+      <div class="dropdown">
+        <button class="dropbtn">About</button>
+        <div class="dropdown-content">
+          <a href="#">Overview</a>
+          <a href="#">Message from the Chairman</a>
+          <a href="#">Mission and Vision</a>
+          <a href="#">History</a>
+          <a href="#">Campus Tour</a>
+          <a href="#">Virtual Tour</a>
+          <a href="#">Board of Governors</a>
+          <a href="#">Staff Directory</a>
+          <a href="#">Gallery</a>
+        </div>
+      </div> 
+      <div class="dropdown">
+        <button class="dropbtn">Students</button>
+        <div class="dropdown-content">
+          <a href="https://docs.google.com/document/d/1GIKFL8BlfDnbIecSqffIvEc6OX5pEC854ho8dBfarF8/edit?usp=sharing" target="_blank">Final Term Syllabus</a>
+          <a href="https://www.dcmspkl.edu.in/user.php?user=students">Student Profile</a>
+          <a href="https://www.dcmspkl.edu.in/user.php?user=attendance">Attendance</a>
+          <a href="https://dcmspkl.coalmentech.com/#/login?returnUrl=%2Fpanel%2Fhome">Student Anacdotal Report</a>
+          <a href="https://pariksha.coalmentech.com/auth/login">Pariksha Portal</a>
+          <!--# <a href="#">School of Art and Design</a>
+          <a href="#">Library</a>
+          <a href="#">Research</a>
+          <a href="#">Accreditation</a> #-->
+        </div>
+      </div> 
+      <a href="#admissions">Admissions</a>
+      <a href="#placements">Placements</a>
+      <a href="#news">News</a>
+      <a href="#events">Events</a>
+      <a href="#contact">Contact</a>
+    </div>
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRjVCCGcGV7SEdIPKnwUeww-07dZh5jbYjAjpVvLW9HKabSDy1gngxNk-OjmgvVMcK8sbmiTAESa7-g/embed?start=false&loop=true&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" Style=float:center;></iframe>
+
+      <TH><Table Border class=bx><TR VAlign=right><TH><B Style=Color:#7E010F Class=bbt><IFrame src=https://dcmspkl.edu.in/user.php?user=calendar Width=100% Height=100% FrameBorder=0 Scrolling=yes Style=BackGround:White;Margin-center:-10px;float:center; my-5></IFrame></Table>
+      <TH><Table Border class=bx><TR VAlign=right><TH><B Style=Color:#7E010F Class=bbt>Happy Birthday 
+<TR><TH Style='background:url(https://i.postimg.cc/vT0Xbzfb/birthday.png);float:right;Background-Color:#EEEEEE;background-repeat:no-repeat;Height:200px;Background-Size:100% 100%;'>
+<DIV Style='float:center;Position:Relative;Left:0px;Top:-0px;Width:250px;Height:180px;Overflow:Hidden;Border-Radius:30%;Opacity:0.9;Border:Solid 1px Grey'>
+<IFrame src=https://dcmspkl.edu.in/user.php?user=birthday width=171px height=180px FrameBorder=No Scrolling=No></IFrame></DIV></Table>
+
+<TH><A href=https://dcmspkl.edu.in/user.php?user=mybtn&fn=@Class4c Target=MyWeb OnClick="window.scrollTo(0,0);"><P class=btn Style=BackGround-Color:#3333CC;width:23%;float:left;><font size=4>CLASS (Learning Interactions)2022-23</P></A>
+<A href=https://dcmspkl.edu.in/user.php?user=mybtn&fn=@Corridor4c Target=MyWeb OnClick="window.scrollTo(0,0);"><P class=btn Style=BackGround-Color:#07a382;width:23%;float:left;><font size=4>CORRIDOR (Immersive Spaces)2022-23</P></A>
+<A href=https://dcmspkl.edu.in/user.php?user=mybtn&fn=@Campus4c Target=MyWeb OnClick="window.scrollTo(0,0);"><P class=btn Style=BackGround-Color:#DB8113;width:23%;float:left;><font size=4>CAMPUS (Enabling Culture)2022-23</P></A>
+<A href=https://dcmspkl.edu.in/user.php?user=mybtn&fn=@Community4c Target=MyWeb OnClick="window.scrollTo(0,0);"><P class=btn Style=BackGround-Color:#30B1CE;width:23%;float:left;><font size=4>COMMUNITY (Outreach)2022-23</P></A>
+
+        
+        <body>
+        <footer>
+        
+        </Table>
+
+        <TH><Table Border class=bx><TR BGColor=#E5C324 ><TH Style=Height:30px Class="bbt fts"><font color=#0B5345>PARENTING TIPS<TR><TH   Style=Width:300px;Height:400px;Overflow:Hidden;Padding:7px;Padding-Top:0;float:right; bgcolor=#D1F7F5>
+<font  size=3 face=Ariel><p align=Justify>       
+	Education is not to be taken as a process of filling an empty sack and pouring out its contents, making the sack empty again. It is not the head that has to be filled through education. It is the heart that has to be cleansed, expanded and illumined. Education is for 'life', not for making a 'living'. The sign of the educated man is humility that he knows little about the vast unknown that still remains to be explored. The educated man must realise that he has more obligations than privileges, and more duties than rights. He has to serve the society amidst which he is placed and the heritage that has been handed down to him by his forefathers. He should be delighted to serve, and not desire to dominate. For service is divine; service makes life worthwhile. Service indeed is the best way to use one's skills, intelligence, strength and resources.</Table>
+
+  <TH><Table Border class=bx><TR BGColor=#E5C324 ><TH Style=Height:30px Class="bbt fts"><font color=#0B5345>DAILY QUIZ <TR><TH   Style=float:right;Width:300px;Height:400px;Overflow:Hidden;;Padding:7px;Padding-Top:0 bgcolor=#D1F7F5>
+<font color=Blue size=4 face=Calibri>
+<marquee direction=up onmouseover=this.stop(); onmouseout=this.start();  Scrollamount=4  Style=Height:350px;float:right;><p align=justify>
+<font color="navy" size=4><br><font color=green>Session 2022-23</font><br>
+<font color=red><u>WORD-I-PEDIA </u></font><br><font size=3>
+An avid way to enhance the vocabulary of our children here we begin a new quizlet. </font>
+A systematic array of questions leading towards eloquence. 
+So ,come on ...Let's join !!!
+Win a positive anecdotal.<br><br>
+
+<font color=green>TO PARTICIPATE IN THE QUIZ</font><br><br>
+<font size=4 color=blue>
+Monday Alternate  -Classes 1st to 4th <br>
+Tuesday Alternate -Classes 5th to 10th  <br></font>
+ <br>
+<font color=red><b>will be resumed after Exams.</b></font>
+  
+
+
+  </Table>
+<TR><TH><a href = "https://techpandeyyt.blogspot.com/" target=_blank><IMG src=https://i.postimg.cc/Pr1L04sw/footer.gif Align=Center></a>
+<TR BGColor=#248f8f Style=Height:109px><TH Align=Center>
+<Table Align=Center ID=ft><TR><TH BGColor=#DB8113 Style=Width:290px;Padding:7px;Border-Radius:10px>
+<Table CellPadding=0 CellSpacing=0>
+<TR><TD><IMG src=https://i.postimg.cc/sxRGWc2R/ltr.png><TD class=bbt><B>
+<A href=user.php?user=contacts Target=MyWeb OnClick=window.scrollTo(0,0);>GET IN-TOUCH<BR>WITH US</B></A>
+</Table>
+<TH class=bbt><B Style=Color:#7E010F>D C Montessori</B><B Style=Font-Size:0.73em;Color:white><BR>(Sr. Sec. SMART) School<TD Style=Width:290;Color:white;Font-Size:1.09em>Pocket No 2 & 3,<BR>Near Modern Housing Complex,<BR>Manimajra - Chandigarh <A href=user.php?user=adminform  Style=Font-Size:0.73em;Color:#FFFFFF Target=_Blank>- UT</A>
+<BR>Phone: 0172 2734464 , 9914774546
+<BR>Mail us at:dcmontessorischool@yahoo.com
+</Table>
+      
+        <Table Align=left ID=ft><TR><TD>&copy; 2009-2023 TechPandeyYT<TD Align=Center><A href=https://techpandeyyt.blogspot.com/p/privacy-policy.html target=_blank Style=Color:#000099>Privacy Policy</A> & <A href=https://techpandeyyt.blogspot.com/p/terms-conditions.html target=_blank Style=Color:#000099>Terms of Use</A><TD Align=Right>Maintenance & Support - <Small>अहिंसा परमो धर्मा:</Table>
+</Table>
+      
+      </footer>
+
